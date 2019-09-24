5f322349660a1a88b0f80beced6606b874b03b90
swmo <aiden.swmo@gmail.com>
2019-09-24 18:29:16 +0900

    Fix minor bug

--- network/masked_mlp.py
+++ network/masked_mlp.py
@@ -1,21 +1,23 @@
-import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 from network.base_model import BaseModel
-from network.masked_modules import MaskedLinear, MaskedConv2d
+from network.masked_modules import MaskedLinear
 
 
 class MaskedMLP(BaseModel):
 	def __init__(self, widths, init_scheme=nn.init.xavier_normal_):
 		super(MaskedMLP, self).__init__(init_scheme)
+		depth = len(widths) - 1
 		layers = []
-		for layer in range(len(widths) - 1):
-			layers += [MaskedLinear(widths[layer], widths[layer + 1], init_scheme=init_scheme),
-			           nn.ReLU(inplace=True)]
+		for layer in range(depth):
+			layers += [MaskedLinear(widths[layer], widths[layer + 1], init_scheme=init_scheme)]
+			if layer < depth - 1:
+				layers += [nn.ReLU(inplace=True)]
 		self.fc = nn.Sequential(*layers)
 
 	def forward(self, x):
-		x = x.reshape(x.size()[0], -1)  # flatten
+		x = x.view(len(x), -1)
 		x = self.fc(x)
+
 		return x
